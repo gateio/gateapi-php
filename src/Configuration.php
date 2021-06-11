c70d2db14241939f915774d695a4b5bf6c194482
Revil Wang <revilwang@gmail.com>
2021-06-11 19:39:52 +0800

    fix inconsistent version

--- src/Configuration.php
+++ src/Configuration.php
@@ -71,7 +71,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'OpenAPI-Generator/6.21.2/PHP';
+    protected $userAgent = 'OpenAPI-Generator/5.21.2/PHP';
 
     /**
      * Debug switch (default set to false)
@@ -388,7 +388,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 4.21.2' . PHP_EOL;
-        $report .= '    SDK Package Version: 6.21.2' . PHP_EOL;
+        $report .= '    SDK Package Version: 5.21.2' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
